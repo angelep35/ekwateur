dada2afb21d30877bd86fec7ecaadfea3dce80de
Angèle Petitjean <angele1802@hotmail.fr>
2024-12-04 13:46:54 +0100

    adding a printBill method

--- src/main/java/fr/ekwateur/angelep/billing/BillingApplication.java
+++ src/main/java/fr/ekwateur/angelep/billing/BillingApplication.java
@@ -1,8 +1,14 @@
 package fr.ekwateur.angelep.billing;
 
+import fr.ekwateur.angelep.billing.model.CorporateClient;
+import fr.ekwateur.angelep.billing.model.Gender;
+import fr.ekwateur.angelep.billing.model.IndividualClient;
+import fr.ekwateur.angelep.billing.model.MonthlyBill;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.math.BigDecimal;
 
 @SpringBootApplication
 public class BillingApplication implements CommandLineRunner {
@@ -11,8 +17,48 @@ public class BillingApplication implements CommandLineRunner {
 		SpringApplication.run(BillingApplication.class, args);
 	}
 
+	private IndividualClient individualClient = new IndividualClient(
+			"EKW000000001",
+			100,
+			250,
+			Gender.FEMALE,
+			"Angele",
+			"Petitjean"
+	);
+
+	private CorporateClient corporateClientUpperTurnover = new CorporateClient(
+			"EKW000000020",
+			3000,
+			6500,
+			"SIRET",
+			"myCompany",
+			BigDecimal.valueOf(2000000)
+	);
+
+	private CorporateClient corporateClientLowerTurnover = new CorporateClient(
+			"EKW000000021",
+			3000,
+			6500,
+			"SIRET",
+			"myCompany",
+			BigDecimal.valueOf(2300)
+	);
+
+	private CorporateClient corporateClient1000000Turnover = new CorporateClient(
+			"EKW000000022",
+			3000,
+			6500,
+			"SIRET",
+			"myCompany",
+			BigDecimal.valueOf(1000000)
+	);
+
+
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hello World!");
+		System.out.println(new MonthlyBill(individualClient).printBill());
+		System.out.println(new MonthlyBill(corporateClient1000000Turnover).printBill());
+		System.out.println(new MonthlyBill(corporateClientLowerTurnover).printBill());
+		System.out.println(new MonthlyBill(corporateClientUpperTurnover).printBill());
 	}
 }
--- src/main/java/fr/ekwateur/angelep/billing/model/MonthlyBill.java
+++ src/main/java/fr/ekwateur/angelep/billing/model/MonthlyBill.java
@@ -4,6 +4,7 @@ import fr.ekwateur.angelep.billing.utils.EnergyPriceUtils;
 import lombok.Getter;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 import static fr.ekwateur.angelep.billing.model.Energy.ELECTRICITY;
 import static fr.ekwateur.angelep.billing.model.Energy.GAS;
@@ -28,5 +29,29 @@ public class MonthlyBill {
 
     private BigDecimal calculateEnergyAmountDue(int consumption, BigDecimal energyPrice) {
         return BigDecimal.valueOf(consumption).multiply(energyPrice);
+    }
+
+    public String printBill() {
+        return String.format(
+                "Monthly Bill for Client: %s\n" +
+                        "---------------------------------------------\n" +
+                        "Gas Consumption:               %d kWh\n" +
+                        "Gas Price:                     %.3f €/kWh\n" +
+                        "Gas Amount Due:                %.2f €\n\n" +
+
+                        "Electricity Consumption:       %d kWh\n" +
+                        "Electricity Price:             %.3f €/kWh\n" +
+                        "Electricity Amount Due:        %.2f €\n" +
+                        "---------------------------------------------\n" +
+                        "Total Amount Due:              %.2f €\n",
+                client.getClientNumber(),
+                client.getMonthlyGasConsumption(),
+                gasPrice.setScale(3, RoundingMode.HALF_DOWN),
+                gasAmountDue.setScale(2, RoundingMode.HALF_DOWN),
+                client.getMonthlyElectricityConsumption(),
+                electricityPrice.setScale(3, RoundingMode.HALF_DOWN),
+                electricityAmountDue.setScale(2, RoundingMode.HALF_DOWN),
+                totalAmountDue.setScale(2, RoundingMode.HALF_DOWN)
+        );
     }
 }
